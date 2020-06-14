35c2dd8cc1c1730efc43c21e7820e9c0812c8d0b
Educorreia932 <skelozard1@gmail.com>
2020-06-14 22:21:49 +0100

    Plotting pokémon sprites instead of bars

--- plot.py
+++ plot.py
@@ -1,11 +1,12 @@
 import csv
 import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.image as mpimg
+import numpy as np
 
 origins = {}
 
 # Read data file
-
 with open('data.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     
@@ -18,16 +19,22 @@ with open('data.csv', newline='') as csvfile:
                 origins[row[1]].append(row[0])
                 
 # Graph plotting
-
 origin_name = list(origins)
 number_of_pokemon = list(len(origin) for origin in origins.values())
 
 df = pd.DataFrame({"x": origin_name, "y": number_of_pokemon})
 df = df.sort_values("y")
 
-plt.xticks(rotation = "vertical")
+plt.xlim(-0.5, len(df.x))
+plt.ylim(0, max(df.y))
+
+plt.xticks(np.arange(len(df.x)), df.x, rotation = "vertical")
 plt.ylabel("Number of Pokémon")
 
-plt.bar(df.x, df.y)
+# Plotting pokémon sprites
+for i, (height, origin_name) in enumerate(zip(df.y, df.x)):
+    for j, pokemon_name in enumerate(origins[origin_name]):
+        image = mpimg.imread("images/" + pokemon_name + ".png")
+        plt.imshow(image, extent=[i - 0.5, i + 0.5, j, j + 1])
 
 plt.show()
