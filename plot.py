db63d4f2c7d9560cbc6c2e76f681a8bf6503a42b
Educorreia932 <skelozard1@gmail.com>
2020-06-14 17:48:59 +0100

    Initial stage of graphical plotting
    Plotting simple bars

--- plot.py
+++ plot.py
@@ -1,16 +1,29 @@
 import csv
+import matplotlib.pyplot as plt
 
-origin = {}
+origins = {}
+
+# Read data file
 
 with open('data.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     
     for row in reader:
-        if row[1] != "":
-            if row[1] not in origin:
-                origin[row[1]] = [row[0]]
+        if row[1] != "" and row[1] != "Origin":
+            if row[1] not in origins:
+                origins[row[1]] = [row[0]]
                 
             else:
-                origin[row[1]].append(row[0])
+                origins[row[1]].append(row[0])
                 
-    print(origin)
+# Graph plotting
+
+origin_name = list(origins)
+number_of_pokemon = list(len(origin) for origin in origins)
+
+plt.bar(origin_name, number_of_pokemon)
+plt.xticks(rotation = "vertical")
+plt.ylabel("Number of pokémon")
+plt.title("Pokémon Origins")
+
+plt.show()
